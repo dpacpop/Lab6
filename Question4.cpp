59f99f71c164c8688eb686598afd7bf6adcf61bd
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-10-21 20:20:30 +0530

    Create Question4.cpp

---
+++ Question4.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+# include<string.h>
+using namespace std;
+int main()
+{
+	
+	char str[]="Dhairya Patel";
+	char* point=str;
+	int size=strlen(str);
+	int i;
+	cout<<"printing string using normal loop method.\n";
+	for(i=0;i<size;i++)
+	cout<<str[i];
+	cout<<"\nprinting the string using pointer.\n";
+	cout<<point;
+	return 0;
+}
