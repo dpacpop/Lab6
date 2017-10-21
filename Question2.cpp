5ce3180a15c72d796f84f47628406c270adaa6ed
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-10-21 19:52:08 +0530

    Create Question2.cpp

---
+++ Question2.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	int* p;
+	p=&a;
+	b=*p;
+	a=2;
+	b=3;
+	cout<<"a= "<<a<<"\nb= "<<b<<"\n*p= "<<*p<<endl;
+	p=&b;
+	cout<<"a= "<<a<<"\nb= "<<b<<"\n*p= "<<*p<<endl;
+}
