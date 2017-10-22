1bbf2925ce3bf91801f790e205108242cf98cd8a
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-10-22 10:06:45 +0530

    Create Question6.cpp

---
+++ Question6.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+# include<stdio.h>
+# include<string.h>
+using namespace std;
+int main(void)
+{
+    char str[20], *p;
+    cout << "Input about 10 characters of string\n";
+    cin >> str;
+    p=str;
+    int i;
+    for(i=9;i>=0;i--)
+    cout<<(p+i)<<endl;
+    return 0;
+}
