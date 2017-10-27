33d0b86a28ef9c65af1c3005bdb3c904b6872380
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-10-27 20:03:39 +0530

    Update Question7e.cpp

--- Question7e.cpp
+++ Question7e.cpp
@@ -4,18 +4,21 @@
 using namespace std;
 int Strchr(char str[],char c)
 {
-	int i=0;
+	int i=0,j=0;
 	while(str[i]!=c)
-	i++;
-	return (i+1);
+	{
+	    i++;
+	    if(str[i]=='\0')
+	    return -1;
+	    return (i+1);
 }
 int main(void)
 {
     char str[100],c;
     cout<<"enter a string(max 100 characters)\n";
     cin>>str;
-	cout<<"enter a character to find it's first occurance in the string\n";
-	cin>>c;
-    cout<<"the character is present at position- "<<Strchr(str,c);
+    cout<<"enter a character to find it's first occurance in the string\n";
+    cin>>c;
+    cout<<"the character is present at position(-1=charaacter not present in the string)- "<<Strchr(str,c);
     return 0;
 }
